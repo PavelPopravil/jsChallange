f010006b7bc2a4bf1c54aaf55894726a56edbfd9
PavelPopravil <pasukmc@gmail.com>
2017-01-05 18:51:57 +0300

    + day-1

---
+++ challanges/day1 - Drum Kit/js/main.js
@@ -0,0 +1,9 @@
+window.addEventListener('keydown', function(e) {
+
+	const audio = document.querySelector('audio[data-key="' + e.keyCode + '"]');
+	const key = document.querySelector('.key[data-key="' + e.keyCode + '"]');
+	audio.play();
+	if (!audio) return;
+	key.classList.add('playing');
+
+});
